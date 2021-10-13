5487f5c2ce141fba587b99b11b122567b7506c8e
Poornima Babu <86886698+itsmepoornima@users.noreply.github.com>
2021-10-13 16:04:40 +0530

    Add files via upload

---
+++ setoperations.py
@@ -0,0 +1,9 @@
+#set operations
+
+e={0,2,4,6,8}
+n={2,4,1,3,5}
+print("Union of e and n is ",e.union(n))
+print("intersection of e and n is",e.intersection(n))
+print("difference of e and n is",e.difference(n))
+print("symmetric difference of e and n is ",e.symmetric_difference(n))
+
